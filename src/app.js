93b682019ffb6e15ea5458f72e2142b2ce5c4fe4
asosunoff <al.sosunoff@gmail.com>
2020-05-30 16:25:12 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const app = require("express")();
+const server = require("http").createServer(app);
+const config = require("./config/");
+const logger = require('./libs/logger')(module);
+
+app.use('/', (req, res) => {
+	res.send('<a href="http://localhost:3001/">Войти на сайт</a>');
+});
+
+server.listen(config.get("port"), () => {
+	// logger.silly(`Сервер запущен на порту ${config.get('port')}`);
+	// logger.debug(`Сервер запущен на порту ${config.get('port')}`);
+	// logger.verbose(`Сервер запущен на порту ${config.get('port')}`);
+	// logger.http(`Сервер запущен на порту ${config.get('port')}`);
+	logger.info(`Сервер запущен на порту ${config.get("port")}`);
+	// logger.warn(`Сервер запущен на порту ${config.get('port')}`);
+	// logger.error(`Сервер запущен на порту ${config.get('port')}`);
+});
